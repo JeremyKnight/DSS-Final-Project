1d438abd7d0716be4f66b38e4c24395b645c5d10
Tyler Gamlem <Tyler@mgamlem3.com>
2020-12-05 16:43:03 -0800

    add beginning edge detection kernel

--- final-project/final-project/kernel.cu
+++ final-project/final-project/kernel.cu
@@ -180,3 +180,84 @@ __host__ void squareBlurLauncher(RGB* pixel, int height, int width) {
 	cudaMemcpy(pixel, d_pixel, height * width * sizeof(RGB), cudaMemcpyDeviceToHost);
 
 }
+
+
+// |Gx(x,y)| = -P(x-1,y-1) + -2 *P(x-1,y) + -P(x-1,y+1) + P(x+1,y-1) + 2 * P(x + 1, y) + P(x + 1, y + 1)
+// |Gy(x,y)| = P(x-1,y-1) + 2*P(x,y-1) + P(x+1,y-1) + -P(x-1,y+1) + –2 * P(x, y + 1) - P(x + 1, y + 1)
+
+__global__ void edgeDetectionKernel(RGB* d_pixels, int height, int width)
+{
+	// determine the current pixel
+	int col = blockIdx.x * blockDim.x + threadIdx.x;
+	int row = blockIdx.y * blockDim.y + threadIdx.y;
+
+	/*float h, s, i;
+	i = (d_pixels[row * width + col].red + d_pixels[row * width + col].green + d_pixels[row * width + col].blue) / 3;
+	h = acos(((d_pixels[row * width + col].red - d_pixels[row * width + col].green) + (d_pixels[row * width + col].red - d_pixels[row * width + col].blue) / (2 * sqrt(((d_pixels[row * width + col].red - d_pixels[row * width + col].green) * (d_pixels[row * width + col].red - d_pixels[row * width + col].green)) + ((d_pixels[row * width + col].red - d_pixels[row * width + col].blue) * (d_pixels[row * width + col].green - d_pixels[row * width + col].blue))))));
+	if (d_pixels[row * width + col].red < d_pixels[row * width + col].green && d_pixels[row * width + col].red < d_pixels[row * width + col].blue) {
+		s = d_pixels[row * width + col].red / i;
+	}
+	else if (d_pixels[row * width + col].green < d_pixels[row * width + col].red && d_pixels[row * width + col].green < d_pixels[row * width + col].blue) {
+		s = d_pixels[row * width + col].green / i;
+	}
+	else {
+		s = d_pixels[row * width + col].blue / i;
+	}
+
+	d_pixels[row * width + col].red = h;
+	d_pixels[row * width + col].green = s;
+	d_pixels[row * width + col].blue = i;*/
+
+	float reddx, reddy;
+	float greendx, greendy;
+	float bluedx, bluedy;
+
+	if (col < width && row < height) {
+		if (col > 0 && row > 0 && col < width && row < height) {
+			//red
+			reddx = (-1 * d_pixels[(row - 1) * width + (col - 1)].red) + (-2 * d_pixels[row * width + (col - 1)].red) + (-1 * d_pixels[(row + 1) * width + (col - 1)].red) + (d_pixels[(row - 1) * width + (col + 1)].red) + (2 * d_pixels[row * width + (col + 1)].red) + (d_pixels[(row + 1) * width + (col + 1)].red);
+			reddy = (d_pixels[(row - 1) * width + (col - 1)].red) + (2 * d_pixels[(row - 1) * width + col].red) + (d_pixels[(row - 1) * width + (col + 1)].red) + (-1 * d_pixels[(row + 1) * width + (col - 1)].red) + (-2 * d_pixels[(row + 1) * width + col].red) + (-1 * d_pixels[(row + 1) * width + (col + 1)].red);
+			
+			//reddx /= 5;
+			//reddy /= 5;
+			
+			d_pixels[row * width + col].red = sqrt((reddx * reddx) + (reddy * reddy));
+			
+			//green
+			greendx = (-1 * d_pixels[(row - 1) * width + (col - 1)].green) + (-2 * d_pixels[row * width + (col - 1)].green) + (-1 * d_pixels[(row + 1) * width + (col - 1)].green) + (d_pixels[(row - 1) * width + (col + 1)].green) + (2 * d_pixels[row * width + (col + 1)].green) + (d_pixels[(row + 1) * width + (col + 1)].green);
+			greendy = (d_pixels[(row - 1) * width + (col - 1)].green) + (2 * d_pixels[(row - 1) * width + col].green) + (d_pixels[(row - 1) * width + (col + 1)].green) + (-1 * d_pixels[(row + 1) * width + (col - 1)].green) + (-2 * d_pixels[(row + 1) * width + col].green) + (-1 * d_pixels[(row + 1) * width + (col + 1)].green);
+			
+			//greendx /= 5;
+			//greendy /= 5;
+			
+			d_pixels[row * width + col].green = sqrt((greendx * greendx) + (greendy * greendy));
+			
+			//blue
+			bluedx = (-1 * d_pixels[(row - 1) * width + (col - 1)].blue) + (-2 * d_pixels[row * width + (col - 1)].blue) + (-1 * d_pixels[(row + 1) * width + (col - 1)].blue) + (d_pixels[(row - 1) * width + (col + 1)].blue) + (2 * d_pixels[row * width + (col + 1)].blue) + (d_pixels[(row + 1) * width + (col + 1)].blue);
+			bluedy = (d_pixels[(row - 1) * width + (col - 1)].blue) + (2 * d_pixels[(row - 1) * width + col].blue) + (d_pixels[(row - 1) * width + (col + 1)].blue) + (-1 * d_pixels[(row + 1) * width + (col - 1)].blue) + (-2 * d_pixels[(row + 1) * width + col].blue) + (-1 * d_pixels[(row + 1) * width + (col + 1)].blue);
+			
+			//bluedx /= 5;
+			//bluedy /= 5;
+			
+			d_pixels[row * width + col].blue = sqrt((bluedx * bluedx) + (bluedy * bluedy));
+		}
+	}
+}
+
+__host__ void d_edge_detection(RGB* pixel, int height, int width)
+{
+	RGB* d_pixel;
+
+	cudaMalloc(&d_pixel, height * width * sizeof(RGB));
+	cudaMemcpy(d_pixel, pixel, height * width * sizeof(RGB), cudaMemcpyHostToDevice);
+
+	dim3 grid, block;
+	block.x = 16;
+	block.y = 16;
+	grid.x = calcBlockDim(width, block.x);
+	grid.y = calcBlockDim(height, block.y);
+
+	edgeDetectionKernel << <grid, block >> > (d_pixel, height, width);
+
+	cudaMemcpy(pixel, d_pixel, height * width * sizeof(RGB), cudaMemcpyDeviceToHost);
+}
